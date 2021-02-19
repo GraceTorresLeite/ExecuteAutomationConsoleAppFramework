7e4f1f8eb8599bb2b8a4daabb41cd9414abb727a
GraceTorresLeite <grace.torresleite@gmail.com>
2021-02-19 15:55:26 -0300

    finish basic class

--- SeleniumDemoFramework/Program.cs
+++ SeleniumDemoFramework/Program.cs
@@ -1,28 +1,51 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SeleniumDemoFramework
 {
     class Program
     {
+
+        //Create reference for browser
+        IWebDriver driver = new ChromeDriver(@"c:\Program Files\Google\Chrome");
+
+
         static void Main(string[] args)
         {
-            //Create reference for browser
-            IWebDriver driver = new ChromeDriver(@"c:\Program Files\Google\Chrome");
-
+            
+        }
+        [SetUp]
+        public void Initialize()
+        {
             //Navigate to Google page
             driver.Navigate().GoToUrl("http://www.google.com");
+            Thread.Sleep(2000);
+            Console.WriteLine("Open URL");
+        }
 
+        [Test]
+        public void ExecuteTest()
+        {
             //Find element
             IWebElement element = driver.FindElement(By.Name("q"));
 
             //Perform 
             element.SendKeys("executeautomation");
+            Console.WriteLine("Execute test");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("Open URL");
         }
     }
 }
