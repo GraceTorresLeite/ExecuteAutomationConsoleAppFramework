61a96b021771bd2f722afd6fb393bcf1d81e6e98
GraceTorresLeite <grace.torresleite@gmail.com>
2021-02-19 13:26:03 -0300

    add project

---
+++ SeleniumDemoFramework/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumDemoFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create reference for browser
+            IWebDriver driver = new ChromeDriver(@"c:\Program Files\Google\Chrome");
+
+            //Navigate to Google page
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            //Find element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform 
+            element.SendKeys("executeautomation");
+        }
+    }
+}
